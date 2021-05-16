2778e89d6dc7449b0ab630580dec764030ffd990
Thomas McNish <mcnishthomas@gmail.com>
2021-05-16 18:07:25 -0400

    code cleanup

--- js/quiz.js
+++ js/quiz.js
@@ -4,7 +4,6 @@
 */
 let correctAnswers = 0;
 
-
 // 2. Store the rank of a player
 let playerRank = '';
 
@@ -17,31 +16,26 @@ let main = document.querySelector('main');
    - Keep track of the number of correct answers
 */
 let answer1 = prompt('What is the name of the Queen of England?').toUpperCase();
-
 if (answer1 === 'Elizabeth'.toUpperCase()) {
   correctAnswers += 1;
 }
 
 let answer2 = prompt('What year did the USA get its independence?');
-
 if (answer2 == 1776) {
   correctAnswers += 1;
 }
 
 let answer3 = prompt('What is the legal drinking age in Germany?');
-
 if (answer3 == 16) {
   correctAnswers += 1;
 }
 
 let answer4 = prompt('What is the capital of India?').toUpperCase();
-
 if (answer4 === 'New Delhi'.toUpperCase()) {
   correctAnswers += 1;
 }
 
 let answer5 = prompt('When does the narwhal bacon?').toUpperCase();
-
 if (answer5 === 'midnight'.toUpperCase()) {
   correctAnswers += 1;
 }
@@ -56,16 +50,13 @@ if (answer5 === 'midnight'.toUpperCase()) {
 
 if (correctAnswers == 5) {
   playerRank = 'a Golden Crown 👑';
-} else if (correctAnswers == 3 || correctAnswers == 4) {
+} else if (correctAnswers >= 3) {
   playerRank = 'a Silver Crown';
-} else if (correctAnswers == 2 || correctAnswers == 1) {
+} else if (correctAnswers >= 2) {
   playerRank = 'a Bronze Crown';
 } else {
   playerRank = 'NOTHING! GOOD DAY, SIR!'
 }
-
-
-
 
 // 6. Output results to the <main> element
 main.innerHTML = `<h2>You got ${correctAnswers} correct answers!</h2> <h1>You get ${playerRank} </h1> `;
