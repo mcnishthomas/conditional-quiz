aeb30f13707184f03b2fcf2d6cbbd44129dc9640
Thomas McNish <mcnishthomas@gmail.com>
2021-05-16 17:42:41 -0400

    added questions and beginning of output

--- js/quiz.js
+++ js/quiz.js
@@ -6,7 +6,7 @@ let correctAnswers = 0;
 
 
 // 2. Store the rank of a player
-let playerRank = 0;
+let playerRank = '';
 
 // 3. Select the <main> HTML element
 let main = document.querySelector('main');
@@ -16,11 +16,17 @@ let main = document.querySelector('main');
    - Store each answer in a variable
    - Keep track of the number of correct answers
 */
-let answer1 = prompt('How old is Chewy?');
-if answer1 = 3 {
-  correctAnswers +=
+let answer1 = prompt('What is the name of the Queen of England?').toUpperCase();
+
+if (answer1 === 'Elizabeth'.toUpperCase()) {
+  correctAnswers += 1;
 }
 
+let answer2 = prompt('What year did the USA get its independence?');
+
+if (answer2 == 1776) {
+  correctAnswers += 1;
+}
 
 /*
   5. Rank player based on number of correct answers
@@ -31,5 +37,7 @@ if answer1 = 3 {
 */
 
 
-// 6. Output results to the <main> element
 
+
+// 6. Output results to the <main> element
+main.innerHTML = `<h3>You got ${correctAnswers} correct answers!</h3> <h2>You get a ${}</h2>`;
